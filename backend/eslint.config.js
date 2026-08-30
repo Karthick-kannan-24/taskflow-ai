88cb5a4ebdb3cf80b16c366bfd42c65697cb8175
Karthick kannan <karthick2421997@gmail.com>
2026-08-30 15:20:04 +0530

    Add ESLint flat config

---
+++ backend/eslint.config.js
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: globals.node,
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+    },
+  },
+];
